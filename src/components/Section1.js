7a3ba74e1d4a24f856f1fa040ba644da3e05f8b2
shifttogit-deepanshu <sharmadeepanshu.kaizen1@gmail.com>
2022-01-31 03:49:58 +0530

    setup iframe

--- src/components/Section1.js
+++ src/components/Section1.js
@@ -4,7 +4,7 @@ import "../styles/Sections.css"
 const Section1 = ()=>{
     return (
         <div className="section1-container">
-            <iframe src="http://127.0.0.1:5000/" height={"100%"} width={"100%"}/>
+            <iframe src="https://nolariatest.azurewebsites.net" height={"100%"} width={"100%"} title="python-frame"/>
         </div>
     )
 }
