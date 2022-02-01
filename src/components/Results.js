2d01d8f069855bdfce10d38285f89ea8dbcd253f
shifttogit-deepanshu <sharmadeepanshu.kaizen1@gmail.com>
2022-02-01 21:24:48 +0530

    updated cell directory

--- src/components/Results.js
+++ src/components/Results.js
@@ -9,7 +9,7 @@ const Results = ()=>(
             <h3>2. The images will be fed into the AI (on this website) to detect malarial cells.</h3>
             <h3>3. We have created this website to provide a glimpse of our AI. The full operation will be performed on our IOT device and Android application.</h3>
             <h3>4. Please Upload cell images as input. </h3>
-            <h3>5. You can download few cell images from the folder <span className="url">/cell images </span>so there shall be no need to search for.</h3>
+            <h3>5. You can download few cell images from the folder <span className="url">https://1drv.ms/u/s!AjOpwoGi3yCRhWj-Tsa4mz4MBZOj?e=cuJmN9</span>so there shall be no need to search for.</h3>
             <h3>6. Our AI hasn't been trained on the provided images.</h3>
         </div>      
     </div>
